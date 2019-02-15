462d92aca717b7969add6ad41bcca5e0d7aa5595
Bin Lu <lu.bin.lu.bin@gmail.com>
2019-02-15 15:42:19 -0500

    add more comments

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
-// const friends = [
-//   { id: 1, name: 'Sting', nearMe: true },
-//   { id: 2, name: 'Radiohead', nearMe: true },
-//   { id: 3, name: 'NIN', nearMe: false },
-//   { id: 4, name: 'Echo', nearMe: true },
-//   { id: 5, name: 'Zeppelin', nearMe: false }
-// ];
-// const isNearMe = ({ nearMe }) => nearMe;
-// const getName = ({ name }) => name;
+const friends = [
+  { id: 1, name: 'Sting', nearMe: true },
+  { id: 2, name: 'Radiohead', nearMe: true },
+  { id: 3, name: 'NIN', nearMe: false },
+  { id: 4, name: 'Echo', nearMe: true },
+  { id: 5, name: 'Zeppelin', nearMe: false }
+];
+const isNearMe = ({ nearMe }) => nearMe;
+const getName = ({ name }) => name;
 
 // const map = (a => b) => step => reducer;
 
-// const getFriendsNearMe = compose(
-//   filter(isNearMe),
-//   map(getName)
-// );
 // const results2 = toArray(getFriendsNearMe, friends);
 
-const compose = (...fns) => x => fns.reduceRight((y, f) => f(y), x);
+const compose = (...fns) => x => fns.reduceRight((y, f) => f(y), x); // x is a reducer
 
-// step is a reducer
-// map, filter, doubleEvens are transducer
+// map, filter are higher-order transducer
+// map(double), filter(isEven) are composable:
+//  since both have the signature, reducer => reducer
 
-const map = f => step => (a, c) => step(a, f(c));
+// step is a reducer, (accumulator, current) => accumulator
+
+const map = f => step => (a = step(), c) => step(a, f(c));
 const filter = predicate => step => (a, c) => (predicate(c) ? step(a, c) : a);
 
 const isEven = n => n % 2 === 0;
@@ -30,11 +29,28 @@ const double = n => n * 2;
 const doubleEvens = compose(
   filter(isEven),
   map(double)
+); // doubleEvens is a transducer
+
+// (a, c) => step(a, double(c));
+
+// (a, c) => (predicate(c) ? (a, c) => step(a, double(c))(a, c) : a);
+
+const arrayConcat = (a, c) => {
+  if (!a.concat) {
+    return [c]; //this is bad actually
+  } else {
+    return a.concat([c]); // arrayConcat is the step, a reducer
+  }
+  // console.log(a);
+};
+const xform = doubleEvens(arrayConcat); // xform is a reducer
+
+const result1 = [1, 2, 3, 4, 5, 6].reduce(
+  (a, c) => (isEven(c) ? arrayConcat(a, double(c)) : a),
+  []
 );
 
-const arrayConcat = (a, c) => a.concat([c]);
-const xform = doubleEvens(arrayConcat);
+const result2 = [1, 2, 3, 4, 5, 6].reduce(xform, []); // [4, 8, 12]
 
-const result = [1, 2, 3, 4, 5, 6].reduce(xform, []); // [4, 8, 12]
-
-console.log(result);
+const result3 = [1, 2, 3, 4, 5, 6].reduce(xform); // [4, 8, 12]
+console.log(result1, result2, result3);
